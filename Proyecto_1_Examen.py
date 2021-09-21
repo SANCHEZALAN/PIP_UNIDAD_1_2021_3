2382665e5d79646eb0f36a90b918715e562afc12
Jorge Rodriguez <jorgerdz2806@gmail.com>
2021-09-20 20:36:18 -0500

    Update Proyecto_1_Examen.py

--- Proyecto_1_Examen.py
+++ Proyecto_1_Examen.py
@@ -15,21 +15,40 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # Área de los Signals
+        self.incremento=1
+        self.calif=0
+        self.resul=0
+        self.result=0
         self.num1.setText(str(random.randint(1, 9)))
         self.num2.setText(str(random.randint(1, 9)))
+        self.numi.setText(str(self.incremento))
         self.resultado.clicked.connect(self.examen)
 
     # Área de los Slots
     def examen(self):
+        if(self.resultado.text()=="Calificar"):
             num1 =int(self.num1.text())
             num2 =int(self.num2.text())
-
-            result = int(num1*num2)
-            resultado=int(self.respuesta.text())
-
-            if (resultado==result):
+            self.result = int(num1*num2)
+            self.resul=int(self.respuesta.text())
+            if (self.resul==self.result):
                 self.mensaje("Esta Correcto")
+                self.calif+=1
             else: self.mensaje("Esta Incorrecto")
+            self.respuesta.clear()
+            self.num1.setText(str(random.randint(1, 9)))
+            self.num2.setText(str(random.randint(1, 9)))
+            if (self.incremento == 9):
+                self.resultado.setText("Calif Final")
+            self.incremento += 1
+            self.numi.setText(str(self.incremento))
+        else:
+            if (self.resul==self.result):
+                self.mensaje("Esta Correcto")
+                self.calif+=1
+            else: self.mensaje("Esta Incorrecto")
+            self.mensaje("Tu calificacion es: "+str(self.calif))
+
 
     def mensaje(self,msj):
         m = QtWidgets.QMessageBox()
